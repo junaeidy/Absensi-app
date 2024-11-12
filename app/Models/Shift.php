38fd3587db6f450a188fe3f778c08fa6fff5af40
junaeidy <junaeidys0@gmail.com>
2024-11-12 23:14:16 +0700

    Create Shift and Office

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Shift extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'start_time',
+        'end_time',
+    ];
 }
