83fb38536fc9b459e76771f7819af08c1b9b9d2f
junaeidy <junaeidys0@gmail.com>
2024-11-14 11:30:14 +0700

    create view map in presensi page

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Absensi App</title>
+    <style>
+        #map{
+            height: 400px;
+        }
+    </style>
 </head>
 <body>
-    
+    {{$slot}}
 </body>
 </html>
