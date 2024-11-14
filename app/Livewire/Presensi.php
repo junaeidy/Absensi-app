c3fda0c3e6264e01f8d530db22d4c3ba84389230
junaeidy <junaeidys0@gmail.com>
2024-11-15 00:59:12 +0700

    Radius Validation

--- app/Livewire/Presensi.php
+++ app/Livewire/Presensi.php
@@ -8,11 +8,16 @@ use Illuminate\Support\Facades\Auth;
 
 class Presensi extends Component
 {
+    public $latitude;
+    public $longitude;
+    public $insideRadius = false;
+
     public function render()
     {
         $schedule = Schedule::where('user_id', Auth::user()->id)->first();
         return view('livewire.presensi', [
-            'schedule' => $schedule
+            'schedule' => $schedule,
+            'insideRadius' => $this->insideRadius,
         ]);
     }
 }
