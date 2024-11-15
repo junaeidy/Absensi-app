69fb6e6e560cd8fe8d7a739bbc2fee1fcd5124f8
junaeidy <junaeidys0@gmail.com>
2024-11-15 08:47:58 +0700

    implement create presensi

--- app/Livewire/Presensi.php
+++ app/Livewire/Presensi.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Models\Attendance;
 use Livewire\Component;
 use App\Models\Schedule;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class Presensi extends Component
@@ -15,9 +17,52 @@ class Presensi extends Component
     public function render()
     {
         $schedule = Schedule::where('user_id', Auth::user()->id)->first();
+        $attendance = Attendance::where('user_id', Auth::user()->id)
+                        ->whereDate('created_at', date('Y-m-d'))->first();
         return view('livewire.presensi', [
             'schedule' => $schedule,
             'insideRadius' => $this->insideRadius,
+            'attendance' => $attendance,
         ]);
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'latitude' => 'required',
+            'longitude' => 'required'
+        ]);
+
+        $schedule = Schedule::where('user_id', Auth::user()->id)->first();
+
+        if ($schedule) {
+            $attendance = Attendance::where('user_id', Auth::user()->id)
+                         ->whereDate('created_at', date('Y-m-d'))->first();
+            
+            if(!$attendance) {
+                $attendance = Attendance::create([
+                    'user_id' => Auth::user()->id,
+                    'schedule_latitude' => $schedule->office->latitude,
+                    'schedule_longitude' => $schedule->office->longitude,
+                    'schedule_start_time' => $schedule->shift->start_time,
+                    'schedule_end_time' => $schedule->shift->end_time,
+                    'latitude' => $this->latitude,
+                    'longitude' => $this->longitude,
+                    'start_time' => Carbon::now()->toTimeString(),
+                    'end_time' => Carbon::now()->toTimeString(),
+                ]);
+            }else{
+                $attendance->update([
+                    'latitude' => $this->latitude,
+                    'longitude' => $this->longitude,
+                    'end_time' => Carbon::now()->toTimeString(),
+                ]);
+            }
+
+            return redirect()->route('presensi',[
+                'schedule' => $schedule,
+                'insideRadius' => false,
+            ]);
+        }
     }
 }
--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'schedule_latitude',
+        'schedule_longitude',
+        'schedule_start_time',
+        'schedule_end_time',
+        'latitude',
+        'longitude',
+        'start_time',
+        'end_time',
+    ];
 }
