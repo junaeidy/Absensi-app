1ed8dfe9e1ed40e4198725d7172cd2ec0c1ce573
junaeidy <junaeidys0@gmail.com>
2024-11-18 16:45:53 +0700

    create Attendance(Month, Year), Banned account, images and more

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::get('/get-attendance-today', [AttendanceController::class, 'getAttendanceToday']);
     Route::get('/get-schedule', [AttendanceController::class, 'getSchedule']);
+    Route::post('/store-attendance', [AttendanceController::class, 'store']);
+    Route::get('/get-attendance-by-month-year/{month}/{year}', [AttendanceController::class, 'getAttendanceByMonthYear']);
+    Route::post('/banned', [AttendanceController::class, 'banned']);
+    Route::get('/get-image', [AttendanceController::class, 'getImage']);
 });
 
 Route::get('/user', function (Request $request) {
