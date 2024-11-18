136dcf0f9388754f7fdbd9b5fee605fc1a842f56
junaeidy <junaeidys0@gmail.com>
2024-11-18 16:16:59 +0700

    create get attendance API

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\AttendanceController;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::get('/get-attendance-today', [AttendanceController::class, 'getAttendanceToday']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
